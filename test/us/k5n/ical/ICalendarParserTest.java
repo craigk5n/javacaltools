e4c5c2f6ab1a0b6183d20adf15ff9f8dc4c56afe
Craig Knudsen <craig@k5n.us>
2007-04-09 15:13:49 +0000

    cleanup

--- test/us/k5n/ical/ICalendarParserTest.java
+++ test/us/k5n/ical/ICalendarParserTest.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.Vector;
 
 import junit.framework.Test;
